508c3e801168742013eb15c4bca0ff1cba73b828
Bruce Williams <bruce@codefluency.com>
2010-02-12 14:48:28 -0800

    MVP.  Possibly 'works'

---
+++ lib/puppet/provider/boot/grubby.rb
@@ -0,0 +1,62 @@
+Puppet::Type.type(:boot).provide(:grubby) do
+
+  commands :grubby => '/sbin/grubby'
+  
+  def create
+    options = ['--add-kernel', @resource.value(:kernel),
+               '--title', @resource.value(:name),
+               '--args', @resource.value(:options)]
+    if @resource.value(:initrd)
+      options.push('--initrd', @resource.value(:initrd))
+    end
+    grubby(*options)
+  end
+
+  def exists?
+    record
+  rescue Puppet::ExecutionFailure
+    false
+  end
+
+  def destroy
+    grubby('--remove-kernel', @resource.value(:kernel))
+  end
+
+  private
+
+  def record
+    @record ||= records.detect do |record|
+      next unless record['kernel'] == @resource.value(:kernel)
+      next unless record['initrd'] == @resource.value(:initrd)
+      next unless record['args'] == @resource.value(:options)
+      true
+    end
+  end
+
+  def records
+    @records ||= find_records
+  end
+
+  def find_records
+    current_index = nil
+    raw_info.split(/\n/).inject([]) do |sets, line|
+      key, value = line.split('=', 2)
+      case key
+      when 'index'
+        if value != current_index
+          sets.push({})
+          current_index = value
+        end
+      when 'args'
+        args = args[1..-2]
+      end
+      sets.last[key] = value
+      sets
+    end
+  end
+
+  def raw_info
+    grubby('--info', @resource.value(:kernel)).chomp
+  end
+
+end
---
+++ lib/puppet/type/kernel.rb
@@ -0,0 +1,34 @@
+require 'pathname'
+
+Puppet::Type.newtype(:boot) do
+
+  ensurable
+  
+  newparam(:name) do
+    isnamevar
+  end
+
+  newparam(:kernel) do
+    isrequired
+    munge do |value|
+      path = Pathname.new(value)
+      unless path.absolute?
+        raise ArgumentError, "kernel must be an absolute path"
+      end
+      path
+    end
+  end
+  
+  newparam(:initrd) do
+    munge do |value|
+      path = Pathname.new(value)
+      unless path.absolute?
+        raise ArgumentError, "kernel must be an absolute path"
+      end
+      path
+    end
+  end
+  
+  newparam(:options)
+
+end
